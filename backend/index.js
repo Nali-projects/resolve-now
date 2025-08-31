2d0428368fcf66b792727ab93c4be0502359c2cf
Nali-projects <knowledgeseekers41214@gmail.com>
2025-08-31 12:01:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const io=new Server(server,{
 app.use(cors());
 app.use(express.json());
 // MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/complaintdb")
+mongoose.connect("mongodb+srv://knowledgeseekers41214_db_user:Nali12##@cluster0.herxn7r.mongodb.net/")
     .then(() => console.log("Connected to database"))
     .catch((err) => console.log("Not connected", err));
 
@@ -275,3 +275,4 @@ const port =3000;
 server.listen(port, () => {
     console.log("Server is running on port " + port);
 });
+
